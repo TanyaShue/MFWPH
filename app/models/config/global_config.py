c84ad362feaecff8e56e6fe5214811a049b10a8a
disjoint7 <disjoint7@163.com>
2025-04-20 23:22:47 +0800

    chore: 初步添加切换配置功能

--- app/models/config/global_config.py
+++ app/models/config/global_config.py
@@ -42,6 +42,11 @@ class GlobalConfig:
         从 JSON 文件中加载全局 AppConfig 配置。
         """
         self.app_config = AppConfig.from_json_file(file_path)
+        self.app_config.migrate_old_resources()
+        print(self.app_config.devices)
+        for device in self.app_config.devices:
+            for resource in device.resources:
+                print(resource.selected_tasks)
 
     def load_resource_config(self, file_path: str) -> None:
         """
